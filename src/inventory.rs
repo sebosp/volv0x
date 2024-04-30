bd5d41cc99796f930754abe3e8e75787c59101c9
Seb Ospina <kraige@gmail.com>
2024-04-30 21:06:44 +0200

    Change name

--- src/inventory.rs
+++ src/inventory.rs
@@ -1,4 +1,4 @@
 //! The inventory module contains the known units and their organelles.
 //!
 
-pub const C6_BUZZER_01: [u8; 6] = [0x00, 0x00, 0x00, 0x00, 0x00, 0x06];
+pub const C6_VIBRATOR_01: [u8; 6] = [0x00, 0x00, 0x00, 0x00, 0x00, 0x06];
